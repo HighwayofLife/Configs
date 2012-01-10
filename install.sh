23c81608a0df766bea0137803f4f1b983eb674aa
David Lewis <highwayoflife@gmail.com>
2012-01-10 13:23:07 -0800

    Add the .screenrc file. Changed the install script to be more accurate, and back to bash.

--- install.sh
+++ install.sh
@@ -1,28 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-export CONFIGDIR=`pwd`
+SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-if [ -e "~/.bashrc" ]; then
+if [ -e ~/.bashrc ]; then
 	echo "Backing up ~/.bashrc to ~/.bashrc.bak"
 	mv ~/.bashrc ~/.bashrc.bak
 fi
 
-ln -s "$CONFIGDIR/.bashrc" ~/.bashrc
-echo "~/.bashrc linked"
+if [ ! -h ~/.bashrc ]; then
+	ln -s "$SELF_PATH/.bashrc" ~/.bashrc
+	echo "~/.bashrc linked"
+else
+	echo "~/.bashrc is already linked"
+fi
 
-if [ -e "~/.vimrc" ]; then
+if [ -e ~/.vimrc ]; then
 	echo "Backing up ~/vimrc to ~/.vimrc.bak"
 	mv ~/.vimrc ~/.vimrc.bak
 fi
 
-ln -s "$CONFIGDIR/.vimrc" ~/.vimrc
-echo "~/.vimrc linked"
+if [ ! -h ~/.vimrc ]; then
+	ln -s "$SELF_PATH/.vimrc" ~/.vimrc
+	echo "~/.vimrc linked"
+else
+	echo "~/.vimrc is already linked"
+fi
 
-if [ -e "~/.gitconfig" ]; then
+if [ -e ~/.gitconfig ]; then
 	read -p "Do you wish to backup your ~/.gitconfig file and copy over a new one? [y/n]: " gc
 	case $gc in 
 		y|Y) mv ~/.gitconfig ~/.gitconfig.bak
-			cp "$CONFIGDIR/.gitconfig" ~/.gitconfig ;;
+			cp "$SELF_PATH/.gitconfig" ~/.gitconfig ;;
 	esac
 fi
 
